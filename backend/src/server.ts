33975fe26a93314a84931afb43e1fc9f6b1d699c
caladavid <davidcalae@gmail.com>
2025-08-13 23:09:20 -0400

    prepared to deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import path from "path"
+import cookieParser from "cookie-parser";
 
 import connectToMongoDB from './db/connectToMongoDB.js';
 
@@ -14,8 +16,11 @@ import reviewRoutes from './routes/review.routes.js'
 
 const app = express();
 
-dotenv.config()
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve()
+
+dotenv.config()
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -25,11 +30,19 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/reviews', reviewRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 app.listen(PORT, () => {
   connectToMongoDB();
