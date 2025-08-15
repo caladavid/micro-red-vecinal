1c44aed396c61c74f65adcd67bb8f8bbdd35d4ea
caladavid <davidcalae@gmail.com>
2025-08-14 22:54:30 -0400

    app.use home deleted

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,11 +21,6 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve()
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
